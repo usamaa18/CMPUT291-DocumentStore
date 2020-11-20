749b57d58bfd4a38429d904aee93e1a659fe5c26
Usama Ali <usamaa18@gmail.com>
2020-11-20 12:48:20 -0700

    added user report functionality

--- main.py
+++ main.py
@@ -9,11 +9,53 @@ COLLECTION_NAMES = {
     "votes": "Votes.json"
 }
 
-def mainMenu():
+def getQuestionsRep(userID, db):
+    pipeline = [ 
+        {"$match": {"PostTypeId": "1", "Id": userID} },
+        {"$group" : {"_id": {"UserID": "$Id"}, "NumQuestions": {"$sum": 1}, "AvgScore": {"$avg": "$Score"} } } 
+    ]
+    return db.posts.aggregate(pipeline)
+
+def getAnswersRep(userID, db):
+    pipeline = [ 
+        {"$match": {"PostTypeId": "2", "Id": userID} },
+        {"$group" : {"_id": {"UserID": "$Id"}, "NumAnswers": {"$sum": 1}, "AvgScore": {"$avg": "$Score"} } } 
+    ]
+    return db.posts.aggregate(pipeline)
+
+def getVotesRep(userID, db):
+    pipeline = [ 
+        {"$match": {"Id": userID} },
+        {"$group" : {"_id": {"UserID": "$Id"}, "NumVotes": {"$sum": "$Score"}} } 
+    ]
+    return db.posts.aggregate(pipeline)
+
+# check if userID exists in posts, tags, and votes collections
+def userExists(userID, db):
+    query = {"Id": userID}
+    for x in ([db.posts.find_one(query)] + [db.tags.find_one(query)] + [db.votes.find_one(query)]):
+        if x is not None:
+            return True
+    return False
+
+# print analytic info about all posts made by userID
+def printUserReport(userID, db):
+    if (not userExists(userID, db)):
+        print("User ID '" + userID + "' not found!")
+        return
+    questionsRep = getQuestionsRep(userID, db)
+    answersRep = getAnswersRep(userID, db)
+    votesRep = getVotesRep(userID, db)
+    print(list(questionsRep))
+    print(list(answersRep))
+    print(list(votesRep))
+
+def mainMenu(db):
     # TODO
 
-    username = input("User ID (optional): ").strip().lower()
-    print([username])
+    userID = input("User ID (optional): ").strip().lower()
+    if userID != '':
+        printUserReport(userID, db)
     pass
 
 
@@ -60,13 +102,11 @@ if __name__ == "__main__":
         # connecting to server
         client = pymongo.MongoClient('localhost', port)
         # TODO: uncomment this
-        resetDB(client)
-        col = {}
-        for colName in COLLECTION_NAMES:
-            col[colName] = client[DATABASE_NAME][colName]
+        # resetDB(client)
+        db = client[DATABASE_NAME]
 
-        print(col)
+        print(db)
         # TODO: create index
-        mainMenu()
+        mainMenu(db)
         
 
